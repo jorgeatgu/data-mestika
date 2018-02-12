98d55dd10f7284bf759c6593bf967acf54794e3f
Jorge Aznar <jorgeatgu@gmail.com>
2018-02-12 19:29:04 +0100

    incluyendo annotations en la primera grafica

--- src/js/line.js
+++ src/js/line.js
@@ -1,7 +1,6 @@
 function jobYear() {
-    //Dias de lluvia
+
     var barPadding = 2;
-    var datos = [];
 
     var margin = { top: 50, right: 50, bottom: 50, left: 110 },
         width = 1200 - margin.left - margin.right,
@@ -15,106 +14,103 @@ function jobYear() {
         .append("g")
         .attr("transform", "translate(" + (margin.left - margin.right) + "," + margin.top + ")");
 
-    var layerDates = svg.append('g');
+    var parseTime = d3.timeParse("%d-%b-%y");
 
-    var bisectDate = d3.bisector(function(d) {
-        return d.fecha;
-    }).left;
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
 
-    var x = d3.scaleTime()
-        .domain([2008, 2017])
-        .range([0, width]);
-
-    var y = d3.scaleLinear()
-        .domain([0, 3500])
-        .range([height, 0]);
-
-    var xAxis = d3.axisBottom(x)
-        .tickFormat(d3.format("d"))
-        .ticks(10);
+    var valueline = d3.line()
+        .curve(d3.curveCatmullRomOpen)
+        .x(function(d) { return x(d.fecha); })
+        .y(function(d) { return y(d.total); });
 
     var yAxis = d3.axisLeft(y)
         .tickSize(-width)
         .tickFormat(d3.format("d"))
         .ticks(8);
 
-    var area = d3.area()
-        .curve(d3.curveBasis)
-        .x(function(d) {
-            return x(d.fecha);
-        })
-        .y0(height)
-        .y1(function(d) {
-            return y(d.total);
-        });
-
-
     d3.csv("csv/data-ofertas-anyo.csv", function(error, data) {
+        if (error) throw error;
 
-        datos = data;
-
-        datos.forEach(function(d) {
-            fecha = d.fecha;
-            total = d.total;
+        data.forEach(function(d) {
+            d.fecha = parseTime(d.fecha);
+            d.total = +d.total;
         });
+
+        x.domain(d3.extent(data, function(d) { return d.fecha; }));
+        y.domain([0, d3.max(data, function(d) { return d.total; })]);
+
+        var path = svg.append("path")
+            .data([data])
+            .attr("class", "line")
+            .attr("d", valueline)
+            .attr("stroke-width", "2")
+            .attr("fill", "none");
+
+            var totalLength = path.node().getTotalLength();
+
+            console.log(totalLength)
+
+        path
+        .attr("stroke-dasharray", totalLength + " " + totalLength)
+        .attr("stroke-dashoffset", totalLength)
+        .transition()
+        .duration(1500)
+        .ease(d3.easePolyInOut)
+        .attr("stroke-dashoffset", 0);
+
 
         svg.append("g")
             .attr("class", "x-axis")
             .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
+            .call(d3.axisBottom(x));
 
         svg.append("g")
             .attr("class", "y-axis")
             .call(yAxis);
 
-        var areaDates = layerDates.append("path")
-            .data([datos])
-            .attr("class", "area")
-            .attr("d", area)
-            .attr("fill", "#A8D8C9");
-
-
-        svg.append("rect")
-            .attr("width", width)
-            .attr("height", height)
-            .style("fill", "none")
-            .style("pointer-events", "all");
-
-
         //Add annotations
-          var labels = [{
+        var labels = [{
             note: {
-                label: "Primera oferta de UX"              },
-            datos: { fecha: "2010", total: 100 }
-          }].map(function (l) {
-              l.note = Object.assign({}, l.note);
-              l.subject = { radius: 6 };
+                label: "Primera oferta de UX",
+                wrap: 430,
+                align: "middle"
+            },
+            y: 320,
+            x: 120,
+            dy: -240,
+            dx: 0,
+        }].map(function(l) {
+            l.note = Object.assign({}, l.note);
+            l.subject = { radius: 6 };
             return l;
-          });
+        });
 
-
-          window.makeAnnotations = d3.annotation().annotations(labels).type(d3.annotationCalloutCircle).accessors({ x: function x(d) {
-              return x(d.fecha);
+        window.makeAnnotations = d3.annotation().annotations(labels).type(d3.annotationCalloutCircle).accessors({
+            x: function x(d) {
+                return x(d.fecha);
             },
             y: function y(d) {
-              return y(d.total);
+                return y(d.total);
             }
-          }).accessorsInverse({
+        }).accessorsInverse({
             fecha: function fecha(d) {
-              return x.invert(d.x);
+                return x.invert(d.x);
             },
-            close: function close(d) {
-              return y.invert(d.y);
+            total: function total(d) {
+                return y.invert(d.y);
             }
-          }).on('subjectover', function (annotation) {
-              annotation.type.a.selectAll("g.annotation-connector, g.annotation-note").classed("hidden", false);
-            }).on('subjectout', function (annotation) {
-              annotation.type.a.selectAll("g.annotation-connector, g.annotation-note").classed("hidden", true);
-            });
+        }).on('subjectover', function(annotation) {
+            annotation.type.a.selectAll("g.annotation-connector, g.annotation-note").classed("hidden", false);
+        }).on('subjectout', function(annotation) {
+            annotation.type.a.selectAll("g.annotation-connector, g.annotation-note").classed("hidden", true);
+        });
 
-            svg.append("g").attr("class", "annotation-test").call(makeAnnotations);
+        svg.append("g").attr("class", "annotation-test").call(makeAnnotations);
 
-            svg.selectAll("g.annotation-connector, g.annotation-note").classed("hidden", true);
+        svg.selectAll("g.annotation-connector, g.annotation-note").classed("hidden", true);
+
+
     });
 
 }
