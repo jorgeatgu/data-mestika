9a83bdffc7e64cfe0a9934fcb9161ed176f314a6
Jorge Aznar <jorgeatgu@gmail.com>
2018-02-22 22:15:12 +0100

    animando la cuarta grafica

--- src/js/line.js
+++ src/js/line.js
@@ -1,10 +1,8 @@
+var margin = { top: 48, right: 48, bottom: 48, left: 48 },
+    width = 900 - margin.left - margin.right,
+    height = 550 - margin.top - margin.bottom;
+
 function jobYear() {
-
-    var barPadding = 2;
-
-    var margin = { top: 48, right: 48, bottom: 48, left: 48 },
-        width = 850 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
 
     var svg = d3.select('.dm-job-year-graph')
         .append('svg')
@@ -128,9 +126,8 @@ function jobYear() {
 }
 
 function centralizame() {
-    var margin = { top: 50, right: 50, bottom: 50, left: 200 },
-        width = 950 - margin.left - margin.right,
-        height = 600 - margin.top - margin.bottom;
+
+    var margin = { top: 50, right: 50, bottom: 50, left: 200 };
 
     var svg = d3.select('.dm-job-city-graph')
         .append('svg')
@@ -206,12 +203,6 @@ function centralizame() {
 
 function remote() {
 
-    var barPadding = 2;
-
-    var margin = { top: 48, right: 48, bottom: 48, left: 48 },
-        width = 900 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-
     var svg = d3.select('.dm-job-remote-graph')
         .append('svg')
         .attr('class', 'dm-job-remote-chart')
@@ -247,7 +238,7 @@ function remote() {
 
         var path = svg.append("path")
             .data([data])
-            .attr("class", "line")
+            .attr("class", "lines")
             .attr("d", valueline)
             .attr("stroke-width", "1.5")
             .attr("fill", "none");
@@ -287,10 +278,6 @@ function remote() {
 
 function multiple() {
 
-    var margin = { top: 48, right: 48, bottom: 48, left: 48 },
-        width = 1100 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-
     var svg = d3.select('.dm-job-multiple-graph')
         .append('svg')
         .attr('class', 'dm-job-multiple-chart')
@@ -328,7 +315,7 @@ function multiple() {
             .key(function(d) { return d.puesto; })
             .entries(data);
 
-        var colors = ["#769CA6","#9C1B12","#759CA7","#CEBAC6","#2D3065"]
+        var colors = ["#b114c0", "#9C1B12", "#759CA7", "#CEBAC6", "#2D3065"]
 
         var color = d3.scaleOrdinal(colors);
 
@@ -350,6 +337,19 @@ function multiple() {
         svg.append("g")
             .attr("class", "yAxis")
             .call(yAxis);
+
+        d3.selectAll(".line").each(function(d, i) {
+            var totalLength = d3.select('.line').node().getTotalLength();
+
+            d3.selectAll('.line').attr("stroke-dasharray", totalLength + " " + totalLength)
+                .attr("stroke-dashoffset", totalLength)
+                .transition()
+                .duration(4000)
+                .delay(200 * i)
+                .ease(d3.easeLinear)
+                .attr("stroke-dashoffset", 0)
+                .style("stroke-width", 2)
+        })
 
     });
 }
