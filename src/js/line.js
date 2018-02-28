f2ca0643559ba267cd200193d5c1a2f841ed17bc
Jorge Aznar <jorgeatgu@gmail.com>
2018-02-28 22:24:20 +0100

    terminando la grafica sobre flash

--- src/js/line.js
+++ src/js/line.js
@@ -7,7 +7,7 @@ function jobYear(){
     var width,height
     var chartWidth, chartHeight
     var margin
-    var svg = d3.select(".dm-container-graph").append("svg")
+    var svg = d3.select(".dm-job-year-graph")
     var axisLayer = svg.append("g").classed("axisLayer", true)
     var chartLayer = svg.append("g").classed("chartLayer", true)
 
@@ -147,7 +147,7 @@ function jobYear(){
             .call(xAxis);
 
     }
-};
+}
 
 
 function centralizame() {
@@ -370,7 +370,139 @@ function multiple() {
     });
 }
 
+function flashJob(){
+
+    var width,height
+    var chartWidth, chartHeight
+    var margin
+    var svg = d3.select(".dm-job-flash-graph")
+    var axisLayer = svg.append("g").classed("axisLayer", true)
+    var chartLayer = svg.append("g").classed("chartLayer", true)
+
+    var x = d3.scaleTime()
+    var y = d3.scaleLinear()
+
+    var parseTime = d3.timeParse("%d-%b-%y");
+
+
+    d3.csv("csv/data-flash-mes.csv", cast,  main)
+
+
+    function cast(d) {
+        d.fecha = parseTime(d.fecha);
+        d.total = +d.total;
+        return d
+        console.log(d.total)
+    }
+
+    function main(data) {
+        update(data)
+        setReSizeEvent(data)
+    }
+
+
+    function update(data) {
+        setSize(data)
+        drawAxis()
+        drawChart(data)
+    }
+
+    function setReSizeEvent(data) {
+            var resizeTimer;
+
+            window.addEventListener('resize', function (event) {
+
+                if (resizeTimer !== false) {
+                    clearTimeout(resizeTimer);
+                }
+                resizeTimer = setTimeout(function () {
+                    update(data)
+                });
+            });
+        }
+
+
+    function setSize(data) {
+
+        width = document.querySelector(".dm-container-graph").clientWidth
+        height = document.querySelector(".dm-container-graph").clientHeight
+
+        margin = {
+            top: 48,
+            left: 48,
+            bottom: 48,
+            right: 48
+        }
+
+        chartWidth = width - (margin.left+margin.right)
+        chartHeight = height - (margin.top+margin.bottom)
+
+        svg.attr("width", width).attr("height", height)
+        axisLayer.attr("width", width).attr("height", height)
+
+        chartLayer
+            .attr("width", chartWidth)
+            .attr("height", chartHeight)
+            .attr("transform", "translate("+[margin.left, margin.top]+")")
+
+        x.domain(d3.extent(data, function(d) { return d.fecha })).range([0, chartWidth])
+        y.domain([0, d3.max(data, function(d) { return d.total })]).range([chartHeight, 0])
+
+    }
+
+    function drawChart(data) {
+
+        var valueline = d3.line()
+            .x(function(d) { return x(d.fecha); })
+            .y(function(d) { return y(d.total); });
+
+        var selectedLineElm = chartLayer.selectAll(".line")
+            .data([data])
+
+        var newLineElm = selectedLineElm.enter().append("path")
+            .attr("class", "line")
+            .attr("stroke-width", "1.5")
+
+        selectedLineElm.merge(newLineElm)
+            .attr("d", valueline)
+
+    }
+
+    function drawAxis(){
+
+        var yAxis = d3.axisLeft(y)
+            .tickSizeInner(-chartWidth)
+            .tickFormat(d3.format("d"))
+            .ticks(10)
+
+        var selectedYAxisElm = axisLayer.selectAll(".y")
+            .data(["dummy"])
+
+        var newYAxisElm = selectedYAxisElm.enter().append("g")
+            .attr("class", "axis y")
+
+        selectedYAxisElm.merge(newYAxisElm)
+            .attr("transform", "translate("+[margin.left, margin.top]+")")
+            .call(yAxis);
+
+        var xAxis = d3.axisBottom(x)
+
+        var selectedXAxisElm = axisLayer.selectAll(".x")
+            .data(["dummy"])
+
+        var newXAxisElm = selectedXAxisElm.enter().append("g")
+            .attr("class", "axis x")
+
+        selectedXAxisElm.merge(newXAxisElm)
+            .attr("transform", "translate("+[margin.left, chartHeight+margin.top]+")")
+            .call(xAxis);
+
+    }
+
+}
+
 jobYear();
 centralizame();
 remote();
 multiple();
+flashJob();
