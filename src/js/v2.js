f4bda47818d32ae0a0f13e0d81f1d554f30202c2
Jorge Aznar <jorgeatgu@gmail.com>
2018-12-10 21:19:35 +0100

    include new chart

--- src/js/v2.js
+++ src/js/v2.js
@@ -647,12 +647,10 @@ const multipleLines = () => {
         .x(d => x(d.fecha))
         .y0(height)
         .y1(d => y(d.cantidad))
-        .curve(d3.curveBasis);
 
     const line = d3.line()
         .x(d => x(d.fecha))
         .y(d => y(d.cantidad))
-        .curve(d3.curveBasis);
 
     d3.csv("csv/data-line-puestos.csv", type, (error, data) => {
 
@@ -866,3 +864,151 @@ const scrolama = () => {
 };
 
 scrolama();
+
+const area = () => { //Estructura similar a la que utilizan en algunos proyectos de pudding.cool
+    const margin = { top: 24, right: 24, bottom: 24, left: 24 };
+    let width = 0;
+    let height = 0;
+    const chart = d3.select('.dm-job-remote-office-graph');
+    const svg = chart.select('svg');
+    const scales = {};
+    let dataz;
+    const getYear = (stringDate) => stringDate.split('-')[2];
+    let puestos = ["presencial", "remoto"];
+
+    //Escala para los ejes X e Y
+    const setupScales = () => {
+
+        const countX = d3.scaleTime()
+            .domain([2008, 2018]);
+
+        const countY = d3.scaleLinear()
+            .domain([0,
+            d3.max(dataz, d => d.presencial)]);
+
+
+        scales.count = { x: countX, y: countY };
+
+    }
+
+    //Seleccionamos el contenedor donde irán las escalas y en este caso el area donde se pirntara nuestra gráfica
+    const setupElements = () => {
+
+        const g = svg.select('.dm-job-remote-office-graph-container');
+
+        g.append('g').attr('class', 'axis axis-x');
+
+        g.append('g').attr('class', 'axis axis-y');
+
+        g.append('g').attr('class', 'dm-job-remote-office-graph-container-bis');
+
+    }
+
+    //Actualizando escalas
+    const updateScales = (width, height) => {
+        scales.count.x.range([0, width]);
+        scales.count.y.range([height, 0]);
+    }
+
+    //Dibujando ejes
+    const drawAxes = (g) => {
+
+        const axisX = d3.axisBottom(scales.count.x)
+            .tickFormat(d3.format("d"))
+            .ticks(13)
+
+        g.select(".axis-x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(axisX)
+
+        const axisY = d3.axisLeft(scales.count.y)
+            .tickFormat(d3.format("d"))
+            .ticks(5)
+            .tickSizeInner(-width)
+
+        g.select(".axis-y")
+            .call(axisY)
+    }
+
+    const updateChart = (dataz) => {
+        const w = chart.node().offsetWidth;
+        const h = 600;
+
+        width = w - margin.left - margin.right;
+        height = h - margin.top - margin.bottom;
+
+        svg
+            .attr('width', w)
+            .attr('height', h);
+
+        const translate = "translate(" + margin.left + "," + margin.top + ")";
+
+        const g = svg.select('.dm-job-remote-office-graph-container')
+
+        g.attr("transform", translate)
+
+        const area = d3.area()
+            .x(d => scales.count.x(d.fecha))
+            .y0(d => scales.count.y(d[0]))
+            .y1(d => scales.count.y(d[1]))
+            .curve(d3.curveCardinal.tension(0.6));
+        updateScales(width, height);
+
+        const stack = d3.stack()
+            .keys(puestos)
+            .offset(d3.stackOffsetSilhouette)
+            .order(d3.stackOrderInsideOut)
+
+        const dataStack = stack(dataz)
+
+        const container = chart.select('.dm-job-remote-office-graph-container-bis')
+
+        const layer = container.selectAll('path')
+            .data(dataStack);
+
+        const newLayer = layer.enter()
+            .append('path')
+            .attr('class', 'area')
+
+        layer.merge(newLayer)
+            .transition()
+            .duration(600)
+            .ease(d3.easeLinear)
+            .attr('d', d => area(d))
+
+        drawAxes(g)
+
+    }
+
+    const resize = () => {
+        updateChart(dataz)
+    }
+
+    // LOAD THE DATA
+    const loadData = () => {
+
+        d3.csv('csv/remoto-presencial.csv', (error, data) => {
+            if (error) {
+                console.log(error);
+            } else {
+                dataz = data
+                dataz.forEach(d => {
+                    d.fecha = d.fecha;
+                    d.remoto = +d.remoto;
+                    d.presencial = +d.presencial;
+                });
+                setupElements()
+                setupScales()
+                updateChart(dataz)
+            }
+
+        });
+    }
+
+    window.addEventListener('resize', resize)
+
+    loadData()
+
+}
+
+area()
